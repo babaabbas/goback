e2433fe2462470bcb80de70068807a0f3f63d126
playercool <playercool02@gmail.com>
2025-07-29 15:15:18 +0530

    main

--- cmd/STUDENT_API/main.go
+++ cmd/STUDENT_API/main.go
@@ -5,4 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to students ai")
 	fmt.Println("Please use the /api/v1/students endpoint to access student data.")
+	fmt.Println("For more information, visit our documentation at")
+
 }
